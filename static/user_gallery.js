c6b85d17db8d6febd322667842855229fbb04d48
Conor Nugent <conornugent96@gmail.com>
2020-11-15 20:52:57 +0000

    finshed image navbar numbers

--- static/user_gallery.js
+++ static/user_gallery.js
@@ -1,7 +1,7 @@
-var imageCount = 0;
 var pageNumber = 1;
 var rowSize = 3;
 var imagesPerPage = rowSize * 2;
+var selectedFolder = 0;
 
 
 var listOfFinProjGb;
@@ -26,54 +26,38 @@ $(function (){
         }
 
         //need to wait untill json has loaded you idiot
-        firstNImages(brakePoint = imagesPerPage, selFolderVal = 0);
+        drawGallery(pageNumber);
     });
-
-    // work out the number of pages needed then generate a image nav with required number of links 
-    //$("#pageNum0").after('<li id="pageNum1" class="page-item"><a class="page-link" href="#"><span>2</span></a></li>');
 
 
     $("#folderSelect").change(function(){
-        $('#gallery').empty();
-        imageCount=0;
-        rowCount=0;
-        firstNImages(brakePoint = imagesPerPage, selFolderVal = $(this).val() );
+        selectedFolder = $(this).val();
+        pageNumber = 1;
+        drawGallery( pageNumber);
     });
 
-
-
-    $("#prev").on("click", function () {
-        pageNumber--;
-        imageCount =  imageCount- (colCount + imagesPerPage);
-        brakePoint = imagesPerPage * pageNumber;
-
-        if( imageCount < imagesPerPage ||pageNumber == 1){
-            firstNImages(brakePoint , selFolderVal = $('#folderSelect').val() );
-            //console.log("imagesPerPage: " + imagesPerPage + " brakePoint: " +  brakePoint + " pageNumber: " +   pageNumber +  " rowCount: " + rowCount + " colCount: "  + colCount + " imageCount: " + imageCount);
-        }
-        else{
-            firstNImages(brakePoint , selFolderVal = $('#folderSelect').val() );
-        }
+    $("#download").on("click", function(){
+        folderName = $("#folderSelect :selected").text();
+        window.location.replace( "download/" +  folderName );
+        
+        
+        
     });
-
-    $("#next").on("click", function () {
-        next();
-    });
-
-
 
 });
 
-function genNavLinks( ){
+function genNavLinks(){
+    prev = '<li onClick="drawGallery('+(pageNumber -1)+ ')"  class="page-item"> <a id = "prev" class="page-link" href="#" aria-label="Previous"> <span aria-hidden="true">&laquo;</span> <span class="sr-only">Previous</span> </a> </li>';
+    next = '<li onClick="drawGallery('+(pageNumber +1)+ ')" id="nextLi"  class="page-item"> <a id = "next" class="page-link" href="#" aria-label="Next"> <span aria-hidden="true">&raquo;</span> <span class="sr-only">Next</span> </a> </li>';
+    $('.pagination').empty();
+    $('.pagination').prepend(prev);
+    $('.pagination').append(next);
+
     var imageLiMax = 4;
     //folder select might not exist deal with later
     selFolder = listOfFinProjGb[ $('#folderSelect').val() ]; 
     listOfFiles = selFolder["files"];
-    numOfPages =   Math.ceil( listOfFiles.length / imagesPerPage);
-
-    for(var i = 1; i <= numOfPages; i++){
-        $("#pageNum" + i  ).remove();
-    }
+    numOfPages =  Math.ceil( listOfFiles.length / imagesPerPage);
     lowerLimit = pageNumber - imageLiMax;
     var windowSlider = 0;
 
@@ -87,81 +71,61 @@ function genNavLinks( ){
          upperLimit =  numOfPages;
     };
 
-    console.log("low limit " + lowerLimit + " upperLImit "  + upperLimit  + " pageNumber " + pageNumber + " NumPages "   + numOfPages  );
     for(var i = lowerLimit; i <= upperLimit; i++ ){
         if(i == pageNumber){
-            $("#numNav" ).before('<li id="pageNum' + i + '" class="page-item active"><a onClick="clickedNum('+ i +')"  class="page-link" href="#"><span>'+  i  + '</span></a></li>');
+            $("#nextLi").before('<li id="pageNum' + i + '" class="page-item active"><a onClick="drawGallery('+ i +')"  class="page-link" href="#"><span>'+  i  + '</span></a></li>');
         }
         else{
-            $("#numNav" ).before('<li id="pageNum' + i + '" class="page-item"><a onClick="clickedNum('+ i +')"  class="page-link" href="#"><span>'+  i  + '</span></a></li>');
+            $("#nextLi").before('<li id="pageNum' + i + '" class="page-item"><a onClick="drawGallery('+ i +')"  class="page-link" href="#"><span>'+  i  + '</span></a></li>');
         }
     }
 
 
-}
-
-function next(){
-    pageNumber += 1;
-    brakePoint = imagesPerPage * pageNumber;
-    //console.log("imagesPerPage: " + imagesPerPage + " brakePoint: " +  brakePoint + " pageNumber: " +   pageNumber +  " rowCount: " + rowCount +  " imageCount: " + imageCount + " listOfFiles: " + listOfFiles.length );
-    firstNImages(brakePoint , selFolderVal = $('#folderSelect').val() );
-
-}
-
-
-function clickedNum(pageNum){
-    firstNImages(brakePoint , selFolderVal = $('#folderSelect').val() );
-
+    $("#prev").show(); 
+    $("#next").show(); 
+    if( pageNumber == 1){
+        $("#prev").hide(); 
     }
 
-    
+    if( pageNumber == numOfPages){
+        $("#next").hide(); 
+    }
 
 
-function firstNImages(brakePoint, selFolderVal){
-    genNavLinks();
+}
+
+
+
+
+function drawGallery(requestPage){
+    pageNumber = requestPage;
+    pageNumber = requestPage;
     $('#gallery').empty();
-    rowCount=0;
-    colCount=0;
-    selFolder = listOfFinProjGb[selFolderVal];
+    rowCount = 0; 
+
+    selFolder = listOfFinProjGb[selectedFolder];
     folderName = selFolder["folderName"];
     listOfFiles = selFolder["files"];
 
+    // starting point of where we start drawing images
+    firstImage = (pageNumber-1) * imagesPerPage ;
+    lastImage = pageNumber * imagesPerPage;
+    imageIndex = firstImage;
 
-    if(brakePoint >= imageCount ){
-        $("#next").hide();
-        $("#prev").show();
-    }
-
-
-    if(listOfFiles.length > brakePoint ){
-        $("#next").show();
-    }
-
-    if(pageNumber == 1 ){
-        $("#prev").hide();
-    }
-
-    if( pageNumber > 1 ){
-        $("#prev").show();
-    }
-
-    //console.log("ImagesPerPage: " + imagesPerPage + " brakePoint: " +  brakePoint + " pageNumber: " +   pageNumber +  " rowCount: " + rowCount +  " imageCount: " + imageCount + " listOfFiles: " + listOfFiles.length );
-    while( imageCount < brakePoint && imageCount < listOfFiles.length ){
-        fileName = listOfFiles[imageCount];
-        if(  imageCount % rowSize == 0){
+    while( imageIndex < lastImage && imageIndex < listOfFiles.length ){
+        fileName = listOfFiles[imageIndex];
+        if(  imageIndex % rowSize == 0){
             rowCount++;
-            $("#gallery").append($("<div id='row"+rowCount + "'" + " class='row'></div>"));
+            $("#gallery").append($("<div id='row"+rowCount+ "'" + " class='row'></div>"));
         }
 
         var path = "/uploaded_images/" + folderName + "/" + fileName ;
-        //console.log(path + " " + pageNumber);
-        $("#" + "row" + rowCount).append( "<div id ="+ imageCount +" class = 'col'> </div>");
-        $("#" + imageCount  ).append($('<img>',{id: fileName ,src: path, class: "lazy"}))
-        imageCount++
-        colCount ++
 
+        $("#" + "row" + rowCount).append( "<div id ="+ imageIndex +" class = 'col'> </div>");
+        $("#" + imageIndex  ).append($('<img>',{id: fileName ,src: path, class: "lazy"}))
+        imageIndex++
     }
-    //console.log("ImagesPerPage: " + imagesPerPage + " brakePoint: " +  brakePoint + " pageNumber: " +   pageNumber +  " rowCount: " + rowCount +  " imageCount: " + imageCount + " listOfFiles: " + listOfFiles.length + " colCount: " + colCount);
+    genNavLinks();
 };
 
 
